e2453eb96a8795e9c0c643e3fc4051fd16858a14
My-Kairo <marciatsoho@gmail.com>
2022-02-21 16:45:08 +0200

    testing

---
+++ index.js
@@ -0,0 +1,23 @@
+let randomNo = Math.ceil((Math.random() * 100));
+
+const guess_no = document.querySelector(".guessNo");
+const guess_btn = document.querySelector(".guess-btn");
+const display = document.querySelector(".display");
+
+
+function clickedBtn () {
+    const guessNumber = Number(guess_no.value);
+    if ( guessNumber < randomNo) {
+        display.innerHTML = "Guess too low";
+    }else if (guessNumber > randomNo) {
+        display.innerHTML = "Guess too high";
+    }else{
+        guessNumber.innerHTML = `Correct, your secreet number is ${guessNumber}`;
+
+    }
+    setTimeout(function(){
+        display.innerHTML = "";
+    }, 2000);
+}
+
+guess_btn.addEventListener('click', clickedBtn)
