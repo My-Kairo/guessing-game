e44b6fa4b2e7545069cdb6821b8de4480a5720a7
My-Kairo <marciatsoho@gmail.com>
2022-02-22 17:14:41 +0200

    new number generated

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ function clickedBtn () {
         display.innerHTML = "Guess too high";
     }else{
         display.innerHTML = `Correct, your secreet number is ${guessNumber}`;
-
+        game()
     }
+
     setTimeout(function(){
         display.innerHTML = "";
     }, 2000);
 }
+
+var guessNumber = Number(guess_no.value);
+var newGame = `${guessNumber}`;
+ function game(){
+    setTimeout(()=>{
+        randomNo = Math.ceil((Math.random() * 100))
+        display.innerHTML = "New game have started";
+    },5000);
+ }
 
 guess_btn.addEventListener('click', clickedBtn)
